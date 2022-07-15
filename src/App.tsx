7a75d872d62c52cc31bd5fe1f879ab66ca0ebff8
Diana Lipanova <altere2804@gmail.com>
2022-07-15 14:59:26 +0300

    Created TopCars component. Training map method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import {TopCars} from "./Components/TopCars";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    // micro task 02:
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+    return (
+        <div className="App">
+            <TopCars topCars={topCars}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/TopCars.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+type TopCarsType = {
+    topCars: Array<CarsType>
+}
+
+type CarsType = {
+    manufacturer: string
+    model: string
+}
+export const TopCars: React.FC<TopCarsType> = ({topCars}) => {
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Manufacturer</th>
+                <th>Model</th>
+            </tr>
+            </thead>
+            <tbody>
+            {topCars.map((c, index) =>
+                <tr key={index}>
+                    {c.manufacturer}
+                    <td>{c.model}</td>
+                </tr>)}
+            </tbody>
+        </table>
+
+    );
+};
